826a61a0e5f823cd67a7ddc79ad6b2eeeffcc5cb
YOY02 <81368814+YOY02@users.noreply.github.com>
2021-04-07 14:03:15 +0200

    Pruebas php 06/04/2021

---
+++ Ejercicio1/Condicionales.php
@@ -0,0 +1,28 @@
+<?php    
+    $edad = 10;
+    $dinero = 0;
+
+    /*
+    if (($edad + $dinero) > 0){
+        echo "Nice, Ahora al Carlos Duty";
+    } else{
+        echo "F, valiste";
+    }
+
+    for ($i = -10; $i < 10; $i ++){
+        if ($i<0){
+            echo "-----| <b>", $i, "</b> |----- </br>";
+        } else {
+            echo "-----| ", $i, " |----- </br>";
+        }
+    }
+    */
+
+    while($edad == 10){
+        echo $edad, " Cumplio años ";
+        $edad = 11; 
+        echo $edad;
+    }
+
+    // w3s
+?>
---
+++ Ejercicio1/intro.php
@@ -0,0 +1,10 @@
+<?php
+    $numero = 10;
+    $num2 = 20;
+
+    echo $numero, "<hr>", $num2, "<hr>";
+    echo $numero + $num2, "</br></br>";
+
+    echo "<b>H</b>ola mundo</br>";
+    echo "Chomusuke";
+?>
